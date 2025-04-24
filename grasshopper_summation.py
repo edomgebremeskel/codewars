6d6d28be50f9517ff9bc5e8cd071ec0fe9a8a9ff
Edom Gebremeskel <edomgebremeskel.efg@gmail.com>
2025-04-24 18:01:27 -0400

    My first Codewars problem

---
+++ grasshopper_summation.py
@@ -0,0 +1,26 @@
+# Description:
+# Summation
+# Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+# For example (Input -> Output):
+
+# 2 -> 3 (1 + 2)
+# 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+
+# My solutions
+def summation(num):
+    value = 0
+    if num> 0:
+        for i in range(1,num+1):
+            value += i
+            i += 1
+    return value
+
+def summation(num):
+    return sum(range(1,num+1))
+
+
+def summation(num):
+    return (1+num) * num / 2
+
